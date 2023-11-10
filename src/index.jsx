c7aded60ab12f0a8cbd94afaaaf3823a1cbcda86
Victor Sepulveda <victorseva123@gmail.com>
2023-11-10 06:46:38 -0500

    Actualizado 10/11/23

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+import createroot from 'react-root';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,7 @@ import { Provider } from 'react-redux';
 import store from './store/index';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+createroot.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
