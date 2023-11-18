9df16a0cd3887665c2ceca583535b5d249eba286
Victor Sepulveda <victorseva123@gmail.com>
2023-11-18 15:54:35 -0500

    Actualizado 18/11/23 optimizando models

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,46 +1,107 @@
+// const { DataTypes } = require('sequelize');
+// // Exportamos una funcion que define el modelo
+// // Luego le injectamos la conexion a sequelize.
+// module.exports = (sequelize) => {
+//   // defino el modelo
+//   sequelize.define('Dog', {
+//     id:{
+//       type: DataTypes.UUID,
+//       allowNull: false,
+//       primaryKey: true
+//     },
+//     name:{
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     height_min:{
+//       type: DataTypes.INTEGER,
+//       allowNull: false
+//     },
+//     weight_min:{
+//       type: DataTypes.INTEGER,
+//       allowNull: false
+//     },
+//     height_max:{
+//       type: DataTypes.INTEGER,
+//       allowNull: false
+//     },
+//     weight_max:{
+//       type: DataTypes.INTEGER,
+//       allowNull: false
+//     },
+//     life_span_min:{
+//       type: DataTypes.INTEGER,
+//       allowNull: true
+//     },
+//     life_span_max:{
+//       type: DataTypes.INTEGER,
+//       allowNull: true
+//     },
+//     image:{
+//       type: DataTypes.STRING,
+//       allowNull: true
+//     },
+//     createdInDB: {
+//       type: DataTypes.BOOLEAN,
+//       allowNull: false,
+//       defaultValue: true
+//     }
+//   });
+// };
+
+
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+
 module.exports = (sequelize) => {
-  // defino el modelo
   sequelize.define('Dog', {
-    id:{
+    // ID único para cada perro, generado automáticamente al crear un perro
+    id: {
       type: DataTypes.UUID,
       allowNull: false,
-      primaryKey: true
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4 // Generar UUID automáticamente al crear un perro
     },
-    name:{
+    // Nombre del perro, no puede ser nulo
+    name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    height_min:{
+    // Altura mínima del perro, no puede ser nula
+    height_min: {
       type: DataTypes.INTEGER,
       allowNull: false
     },
-    weight_min:{
+    // Peso mínimo del perro, no puede ser nulo
+    weight_min: {
       type: DataTypes.INTEGER,
       allowNull: false
     },
-    height_max:{
+    // Altura máxima del perro, no puede ser nula
+    height_max: {
       type: DataTypes.INTEGER,
       allowNull: false
     },
-    weight_max:{
+    // Peso máximo del perro, no puede ser nulo
+    weight_max: {
       type: DataTypes.INTEGER,
       allowNull: false
     },
-    life_span_min:{
+    // Esperanza de vida mínima del perro (opcional)
+    life_span_min: {
       type: DataTypes.INTEGER,
       allowNull: true
     },
-    life_span_max:{
+    // Esperanza de vida máxima del perro (opcional)
+    life_span_max: {
       type: DataTypes.INTEGER,
       allowNull: true
     },
-    image:{
+    // URL de la imagen del perro (opcional)
+    image: {
       type: DataTypes.STRING,
       allowNull: true
     },
+    // Indica si el perro fue creado en la base de datos (predeterminado: true)
     createdInDB: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
