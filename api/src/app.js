ece1ae771e39973ca1437d7e40a33add6d24a9d7
Victor Sepulveda <victorseva123@gmail.com>
2023-11-18 16:26:21 -0500

    Actualizado 18/11/23 optimizndo Routes y app.js

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,43 @@
+// const express = require('express');
+// const cookieParser = require('cookie-parser');
+// const bodyParser = require('body-parser');
+// const morgan = require('morgan');
+// const routes = require('./routes/index.js');
+// const cors = require('cors');
+
+// require('./db.js');
+
+// const server = express();
+
+// server.name = 'API';
+
+// server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+// server.use(bodyParser.json({ limit: '50mb' }));
+// server.use(cookieParser());
+// server.use(morgan('dev'));
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); // update to match the domain you will make the request from
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
+
+// server.use(cors());
+// server.use('/', routes);
+
+// // Error catching endware.
+// server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+//   const status = err.status || 500;
+//   const message = err.message || err;
+//   console.error(err);
+//   res.status(status).send(message);
+// });
+
+// module.exports = server;
+
+
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const cors = require('cors');
@@ -11,27 +48,22 @@ const server = express();
 
 server.name = 'API';
 
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
-server.use(cookieParser());
+// Middlewares
+server.use(cors({
+  origin: 'http://localhost:3001', // Actualiza el origen con la URL correcta
+  credentials: true,
+}));
+server.use(express.json({ limit: '50mb' }));
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
 
-server.use(cors());
+// Routes
 server.use('/', routes);
 
-
-
-// Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+// Error handling middleware
+server.use((err, req, res, next) => {
   const status = err.status || 500;
-  const message = err.message || err;
+  const message = err.message || 'Internal Server Error';
   console.error(err);
   res.status(status).send(message);
 });
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,150 +1,29 @@
-// const { Router} = require('express');
-
-// const axios = require('axios');
-// const {API, API_SEARCH, API_KEY} = process.env;
-// const { Dog, Temperament } = require("../db")
-// const { Op } = require('sequelize');
-// const {formateoDb, formateoApi} = require("../controllers/controllers");
-
+// const { Router } = require('express');
 // // Importar todos los routers;
 // // Ejemplo: const authRouter = require('./auth.js');
-
+// const getDogs = require('../controllers/getDogs')
+// const getIdRaza = require('../controllers/getIdRaza')
+// const getName = require('../controllers/getName')
+// const postDogs = require('../controllers/postDogs')
 
 // const router = Router();
 
 // // Configurar los routers
 // // Ejemplo: router.use('/auth', authRouter);
-
-// // Ruta de prueba
-// router.get('/', (req, res) => {
-//     res.send('¡Hola, mundo!'); 
-//   });
-  
-// // Ruta de prueba
-// // router.get('/dog', (req, res) => {
-// //     res.send('¡Hola, mundo de los perros!');
-// //   });
-
-// router.get('/dog', async(req, res, next) => {
-//     try {
-//     const dogApi = (await axios.get(`${API}?api_key=${API_KEY}`))
-//     const dogDb = await Dog.findAll({include: Temperament});
-
-//     const validandoDogsDb = await formateoDb(dogDb)
-//     const validandoDogsApi = await formateoApi(dogApi)
-
-//     const allDog = await validandoDogsApi.concat(validandoDogsDb)
-//     console.log(dogApi);
-//     return res.status(200).json(allDog)
-    
-    
-
-//     } catch (error) {next(error)}
-// })
-
-// router.get('/dog/:idRaza', async(req, res, next) => {
-//     const {idRaza} = req.params;
-//         if(!idRaza) {
-//         return res.status(400).send({msg: "Falta enviar datos obligatorios"})
-//         }
-
-//     try {
-//     const dogApi = (await axios.get(`${API}?api_key=${API_KEY}`)).data
-//     const dogDb = await Dog.findAll({include: Temperament});
-
-//     const validandoDogsDb = await formateoDb(dogDb)
-//     const validandoDogsApi = await formateoApi(dogApi)
-
-//     const allDog = await validandoDogsDb.concat(validandoDogsApi)
-
-//     const dog = allDog.filter(d => d.id == idRaza)
-
-//     return res.status(200).send(dog)
-//     } catch (error) {next(error)}
-// })
-
-// router.get('/search', async(req, res, next) => {
-//     const {name} = req.query;
-//     try {
-
-//     const dogApi = (await axios.get(`${API}?api_key=${API_KEY}`)).data
-//     const dogDb = await Dog.findAll({where: {name: {[Op.iLike]:`${name}%`}}, include: Temperament})
-
-//     const validandoDogsDb = await formateoDb(dogDb)
-//     const validandoDogsApi = await formateoApi(dogApi)
-
-//     const allDog = await validandoDogsApi.concat(validandoDogsDb)
-//         if(!name){
-//         res.send(allDog)
-//         } else {
-//         const dog = await allDog.filter(d => d.name.toLowerCase().includes(name.toLowerCase()))
-//         return res.status(200).send(dog)
-//         }
-
-//     } catch (error) {next(error)}
-// })
-
-// router.post('/dog', async(req, res) => {
-//     const {name, height_min, height_max, weight_min, weight_max, temperament} = req.body;
-//         if(!name || !height_min || !height_max || !weight_min || !weight_max) {
-//         return res.status(400).send({msg: "Falta enviar datos obligatorios"})
-//         }   
-//     try {
-//     const dog = await Dog.create(req.body)
-
-//     let tempDb = await Temperament.findAll({where: {id : temperament}})
-
-//     await dog.addTemperament(temperament)
-
-//     return res.status(201).send({msg: "Perro creado correctamente"})
-
-//     } catch (error) {console.log(error)}
-// })
-
-// router.get('/temperament', async(req, res, next)=> {
-//     try {
-//     const temperamentos = (await axios.get(`${API}?api_key=${API_KEY}`)).data
-//     const formateo = temperamentos.map(t => t.temperament)
-//     const uniendo = formateo.filter(r => r != null).join().split(", ").join().split(",")
-
-//     let resultado = uniendo.reduce((a, e) => {
-//         if(!a.find(d => d == e)) a.push(e)
-//         return a
-//     }, []);
-
-//     resultado = resultado.map(t => {return{name: t}})
-
-//     const allTemps = await Temperament.findAll()
-    
-//         if(allTemps.length === 0) { await Temperament.bulkCreate(resultado)} 
-//         const temper = await Temperament.findAll()
-      
-//         res.send(temper)
-
-//     } catch (error) {next(error)}
-// })
+// router.get('/dogs', getDogs);
+// router.get('/dogs/:idRaza', getIdRaza);
+// router.get('/name', getName);
+// router.post('/dogs', postDogs)
 
 // module.exports = router;
-///////////////////////////////////////////////////////////////////////////////////
 
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-const getDogs = require('../controllers/getDogs')
-const getIdRaza = require('../controllers/getIdRaza')
-const getName = require('../controllers/getName')
-const postDogs = require('../controllers/postDogs')
-
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-router.get('/dogs', getDogs);
-router.get('/dogs/:idRaza', getIdRaza);
-router.get('/name', getName);
-router.post('/dogs', postDogs)
-
-
-
+// Importar controladores directamente en las rutas para manejar cada endpoint
+router.get('/dogs', require('../controllers/getDogs')); // Endpoint para obtener todos los perros
+router.get('/dogs/:idRaza', require('../controllers/getIdRaza')); // Endpoint para obtener detalles de un perro por ID
+router.get('/name', require('../controllers/getName')); // Endpoint para buscar perros por nombre
+router.post('/dogs', require('../controllers/postDogs')); // Endpoint para agregar un perro
 
 module.exports = router;
