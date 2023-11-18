dfc28c4fa8ca03b09da96b3edc3e665d77ab3789
Victor Sepulveda <victorseva123@gmail.com>
2023-11-18 16:08:42 -0500

    Actualizado 18/11/23 optimizando models

--- api/src/models/Temperament.js
+++ api/src/models/Temperament.js
@@ -1,10 +1,29 @@
-const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
-module.exports = (sequelize) => {
-    // defino el modelo
-    sequelize.define('Temperament', {
+// const { DataTypes } = require('sequelize');
+// // Exportamos una funcion que define el modelo
+// // Luego le injectamos la conexion a sequelize.
+// module.exports = (sequelize) => {
+//     // defino el modelo
+//     sequelize.define('Temperament', {
 
+//         id: {
+//             type: DataTypes.INTEGER,
+//             primaryKey: true,
+//             allowNull: false,
+//             autoIncrement: true
+//         },
+//         name: {
+//             type: DataTypes.STRING,
+//             unique: true
+//         }
+//     }, {timestamps: false})
+// };
+
+const { DataTypes } = require('sequelize');
+
+// Definición del modelo Temperament utilizando Sequelize
+module.exports = (sequelize) => {
+    // Definición del modelo con dos campos: id y name
+    sequelize.define('Temperament', {
         id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
@@ -13,7 +32,10 @@ module.exports = (sequelize) => {
         },
         name: {
             type: DataTypes.STRING,
-            unique: true
+            unique: true, // Asegura que cada nombre de temperamento sea único en la base de datos
+            allowNull: false // Garantiza que el nombre no sea nulo
         }
-    }, {timestamps: false})
+    }, {
+        timestamps: false // Evita la generación de timestamps automáticamente
+    });
 };
