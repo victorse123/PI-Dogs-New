c1e1f5db40a2db4bdd108dddfb9c0c4c4e4c8a7d
Victor Sepulveda <victorseva123@gmail.com>
2023-11-19 14:08:08 -0500

    Actualizado 19/11/23 optimizando front

--- client/src/components/home/Filter/FiltroTemperamento.jsx
+++ client/src/components/home/Filter/FiltroTemperamento.jsx
@@ -1,37 +1,38 @@
 /* eslint-disable no-unused-vars */
-import {useEffect} from "react";
-import {filterDog, getTemperament} from '../../../Redux/actions/actions';
-import {useDispatch, useSelector} from 'react-redux';
+import { useEffect } from "react";
+import { filterDog, getTemperament } from '../../../Redux/actions/actions';
+import { useDispatch, useSelector } from 'react-redux';
 
-function FiltroTemperamento({currentPage, setCurrentPage}) {
+function FiltroTemperamento({ currentPage, setCurrentPage }) {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-
+  // Disparar la acción para obtener los temperamentos al montar el componente
   useEffect(() => {
-    dispatch(getTemperament())
-  }, [dispatch])
+    dispatch(getTemperament());
+  }, [dispatch]);
 
-  const temp = useSelector(state => state.temperaments)
+  // Obtener los temperamentos del estado global utilizando useSelector
+  const temp = useSelector(state => state.temperaments);
 
-  function handleFilter(e){
-    const value = e.target.value
-    setCurrentPage(1)
-    dispatch(filterDog(value))
+  // Manejar el evento onChange del select para filtrar los perros por temperamento
+  function handleFilter(e) {
+    const value = e.target.value;
+    setCurrentPage(1); // Reiniciar la página actual al cambiar el filtro
+    dispatch(filterDog(value)); // Filtrar perros por temperamento
   }
 
-  return(
+  return (
     <div>
       <select onChange={handleFilter}>
-        <option selected disabled>selecciona uno</option>
+        <option selected disabled>Selecciona uno</option>
         <option value="All">All Temperaments</option>
-        {temp && temp.map((t, i) => {
-          return (
-            <option value={t.name} key={i}>{t.name}</option>
-          )
-        })}
+        {/* Mapear los temperamentos y mostrarlos como opciones en el select */}
+        {temp && temp.map((t, i) => (
+          <option value={t.name} key={i}>{t.name}</option>
+        ))}
       </select>
     </div>
-  )
+  );
 }
 
 export default FiltroTemperamento;
