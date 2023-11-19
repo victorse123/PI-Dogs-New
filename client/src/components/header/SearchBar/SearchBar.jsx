4a053d0138fbb0c908085f3e113b7fdc37facdf4
Victor Sepulveda <victorseva123@gmail.com>
2023-11-19 12:20:52 -0500

    Actualizado 19/11/23 optimizando front

--- client/src/components/header/SearchBar/SearchBar.jsx
+++ client/src/components/header/SearchBar/SearchBar.jsx
@@ -1,57 +1,3 @@
-// import { useState } from "react";
-// import {useDispatch, useSelector} from "react-redux";
-// import {searchDogs } from "../../../Redux/actions/actions";
-// import search_icon from "../../../images/Search.png";
-// import './SearchBar.css';
-// import {Link } from "react-router-dom";
-
-// function SearchBar() {
-
-//   const dispatch = useDispatch()
-
-//   const [nameDog, setNameDog] = useState('')
-
-//   const dogsHome = useSelector(state => state.dogsHome)
-
-//   function handleChange(e){
-//     setNameDog(e.target.value)
-//     if(nameDog && nameDog) {
-//       dispatch(searchDogs(nameDog))
-//     }
-//   }
-
-//   function handleClick() {
-//     setNameDog('')
-//   }
-
-//   return(
-//     <div className="searchBar_Container">
-//       <div className="divInput_SearchBar">
-//         <div className="div_button_search">
-//           <img className="searchIcon" src={search_icon} alt="serach" />
-//         </div>
-//         <input className="searchBar" type="text" placeholder="Buscar" onChange={handleChange} value={nameDog}/>
-//         <button className={nameDog.length > 0 ? "cleaner active" : "cleaner"} onClick={handleClick} >x</button>
-//       </div>
-
-//       <div className={nameDog.length !== 0 ? "divSearchBar_Results active" : "divSearchBar_Results"}>
-//         <div className="div_nameResult">
-//           {nameDog && dogsHome.slice(0, 10).map((d, i) => {
-//             return (
-//               <div>
-//                 <Link className="results" to={`/home/${d.id}`} key={i}>{d.name}</Link>
-//               </div>
-//             )
-//           })}
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default SearchBar;
-
-
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { searchDogs } from "../../../Redux/actions/actions";
@@ -64,20 +10,24 @@ function SearchBar() {
   const [nameDog, setNameDog] = useState('');
   const dogsHome = useSelector(state => state.dogsHome);
 
+  // Función para manejar cambios en el input de búsqueda
   function handleChange(e) {
     const name = e.target.value;
     setNameDog(name);
+    // Realiza la búsqueda solo si hay un nombre escrito
     if (name) {
       dispatch(searchDogs(name));
     }
   }
 
+  // Función para limpiar el campo de búsqueda
   function handleClick() {
     setNameDog('');
   }
 
   return (
     <div className="searchBar_Container">
+      {/* Input y botón para la búsqueda */}
       <div className="divInput_SearchBar">
         <div className="div_button_search">
           <img className="searchIcon" src={search_icon} alt="search" />
@@ -89,6 +39,7 @@ function SearchBar() {
           onChange={handleChange}
           value={nameDog}
         />
+        {/* Botón para limpiar el campo de búsqueda */}
         <button
           className={nameDog.length > 0 ? "cleaner active" : "cleaner"}
           onClick={handleClick}
@@ -97,10 +48,12 @@ function SearchBar() {
         </button>
       </div>
 
+      {/* Mostrar resultados de la búsqueda */}
       <div className={nameDog.length !== 0 ? "divSearchBar_Results active" : "divSearchBar_Results"}>
         <div className="div_nameResult">
           {nameDog && dogsHome.slice(0, 10).map((d) => (
             <div key={d.id}>
+              {/* Enlaces de los resultados de búsqueda */}
               <Link className="results" to={`/home/${d.id}`}>{d.name}</Link>
             </div>
           ))}
--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,34 +1,38 @@
 import { useState } from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar/SearchBar';
 import './Header.css';
 import menu from '../../images/Menu.png';
 
-
 function Header() {
+  // Estado para controlar la visibilidad del menú
+  const [menuHo, setMenuHo] = useState(false);
 
-  const [menuHo, setMenuHo] = useState(false)
-
+  // Función para alternar la visibilidad del menú
   function handleClick() {
-    setMenuHo(!menuHo)
+    setMenuHo(!menuHo);
   }
-
-  return(
+  return (
     <div className="header">
       <div className="header_cont">
+        {/* Encabezado para la versión responsive */}
         <div className='div_responsive_header'>
           <Link to="/home" className="logo"><h1 className="logo">DOGS</h1></Link>
           <button className='button_header_menu' onClick={handleClick}>
             <img className='menu' src={menu} alt="menu" />
           </button>
         </div>
-
-        <div className={menuHo  ? "menu_hiden active" : "menu_hiden"}>
-          <a href="https://github.com/villalb4/Dogs" target="_balck" className="about_responsive">GitHub</a>
+        {/* Menú oculto para la versión responsive */}
+        <div className={menuHo ? "menu_hiden active" : "menu_hiden"}>
+          {/* Enlace a tu perfil de GitHub */}
+          <a href="https://github.com/victorse123" target="_balck" className="about_responsive">GitHub</a>
         </div>
 
+        {/* Menú de navegación principal */}
         <div className="nav">
-          <a href="https://github.com/villalb4/Dogs" target="_balck" className="about">GitHub</a>
+          {/* Enlace a tu perfil de GitHub */}
+          <a href="https://github.com/victorse123" target="_balck" className="about">GitHub</a>
+          {/* Componente de barra de búsqueda */}
           <SearchBar />
         </div>
       </div>
