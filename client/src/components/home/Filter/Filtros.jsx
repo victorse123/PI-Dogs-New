a5a47025ce7b201909f2f286489adc949612fb89
Victor Sepulveda <victorseva123@gmail.com>
2023-11-19 13:34:18 -0500

    Actualizado 19/11/23 optimizando front

--- client/src/components/home/Filter/Filtros.jsx
+++ client/src/components/home/Filter/Filtros.jsx
@@ -1,37 +1,46 @@
-import {useState} from "react";
+import { useState } from "react";
 import FiltroTemperamento from "./FiltroTemperamento";
 import FiltroCreado from "./FiltroCreado";
 import './Filtros.css';
 import icon from '../../../images/Icon Filter.png';
 
-function Filtros({currentPage, setCurrentPage}) {
+function Filtros({ currentPage, setCurrentPage }) {
 
-  const [open, setOpen] = useState(false)
+  // Estado para controlar la visibilidad de los filtros
+  const [open, setOpen] = useState(false);
 
-  function handleClick(){
-    setOpen(!open)
+  // Función para manejar el click en el botón de filtro
+  function handleClick() {
+    setOpen(!open);
   }
 
   return (
     <div className="filtro">
+      {/* Botón para abrir y cerrar los filtros */}
       <div className="div_button_filter">
-        <button className="button_filter" onClick={handleClick}><img className="icon_filtro" src={icon} alt="filter" />Filtros</button>
+        <button className="button_filter" onClick={handleClick}>
+          <img className="icon_filtro" src={icon} alt="filter" />Filtros
+        </button>
       </div>
-      {
-        open && 
-        <div className="div_filtros_relative">  
+
+      {/* Mostrar los filtros si 'open' es verdadero */}
+      {open && (
+        <div className="div_filtros_relative">
           <div className="div_filtros">
+            {/* Filtro por fecha de creación */}
             <div className="div_fil">
               <span className="filtro_name">Creado en</span>
               <FiltroCreado currentPage={currentPage} setCurrentPage={setCurrentPage} />
             </div>
+
+            {/* Filtro por temperamentos */}
             <div className="div_fil">
               <span className="filtro_name">Temperamentos</span>
               <FiltroTemperamento currentPage={currentPage} setCurrentPage={setCurrentPage} />
             </div>
           </div>
         </div>
-      }
+      )}
     </div>
   )
 }
