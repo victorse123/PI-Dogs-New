dc5ec92fd2c0a7597d17a7ae71a2b62cfcbef687
Victor Sepulveda <victorseva123@gmail.com>
2023-11-19 16:40:52 -0500

    Actualizado 19/11/23 optimizando front

--- client/src/components/home/Pagination/Pagination.jsx
+++ client/src/components/home/Pagination/Pagination.jsx
@@ -1,64 +1,70 @@
-import {useState} from "react";
+import { useState } from "react";
 import "./Pagination.module.css";
 
-function Pagination({dogsPerPage, totalPosts, paginate, currentPage, setCurrentPage}) {
-
-  const [pageNumberLimit,/*  setPageNumberLimit */] = useState(5);
+function Pagination({ dogsPerPage, totalPosts, paginate, currentPage, setCurrentPage }) {
+  const [pageNumberLimit] = useState(5);
   const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5);
   const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
 
-  // console.log(currentPage)
-  // console.log(setCurrentPage)
-  
+  // Generar números de página
   const pageNumbers = [];
-
-  for(let i = 1; i <= Math.ceil(totalPosts / dogsPerPage); i++) {
-    pageNumbers.push(i)
+  for (let i = 1; i <= Math.ceil(totalPosts / dogsPerPage); i++) {
+    pageNumbers.push(i);
   }
 
+  // Manejar el botón de "Siguiente"
   function handleNext() {
-    if(currentPage !== pageNumbers.length){
-      setCurrentPage(currentPage + 1)
+    if (currentPage !== pageNumbers.length) {
+      setCurrentPage(currentPage + 1);
     }
 
-    if(currentPage + 1 >maxPageNumberLimit) {
+    if (currentPage + 1 > maxPageNumberLimit) {
       setMaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
       setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit);
     }
   }
 
+  // Manejar el botón de "Anterior"
   function handlePrev() {
-    if(currentPage !== 1) {
-      setCurrentPage(currentPage - 1)
+    if (currentPage !== 1) {
+      setCurrentPage(currentPage - 1);
 
-      if((currentPage - 1) % pageNumberLimit === 0) {
+      if ((currentPage - 1) % pageNumberLimit === 0) {
         setMaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
         setMinPageNumberLimit(minPageNumberLimit - pageNumberLimit);
       }
-
     }
   }
 
-
-  return(
+  return (
     <div className="pagination_component">
       <ul className="pagination">
-        <button className="page" onClick={handlePrev}>Prev</button>
-        {pageNumbers && pageNumbers.map((page, i) => {
-          if(page < maxPageNumberLimit+1 && page > minPageNumberLimit) {
-            return(
-              <li key={i} className="pagination_item">
-                <span className={currentPage === page ? "page active" : "page"} onClick={() => paginate(page)}>{page}</span>
-              </li>
-            )
-          } else {
-            return null
-          }
-        })}
-        <button className="page" onClick={handleNext}>Next</button>
+        <button className="page" onClick={handlePrev}>
+          Prev
+        </button>
+        {pageNumbers &&
+          pageNumbers.map((page, i) => {
+            if (page < maxPageNumberLimit + 1 && page > minPageNumberLimit) {
+              return (
+                <li key={i} className="pagination_item">
+                  <span
+                    className={currentPage === page ? "page active" : "page"}
+                    onClick={() => paginate(page)}
+                  >
+                    {page}
+                  </span>
+                </li>
+              );
+            } else {
+              return null;
+            }
+          })}
+        <button className="page" onClick={handleNext}>
+          Next
+        </button>
       </ul>
     </div>
-  )
+  );
 }
 
 export default Pagination;
